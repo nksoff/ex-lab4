f19b9684725ebadcf6e49f6f18f858460689baee
Nikita <nks.off@gmail.com>
2016-10-14 13:59:09 +0300

    add ex 5

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -6,3 +6,17 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+
+import time
+
+
+class timer:
+    @staticmethod
+    def get_time():
+        return time.time()
+
+    def __enter__(self):
+        self.time_start = __class__.get_time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(__class__.get_time() - self.time_start)
