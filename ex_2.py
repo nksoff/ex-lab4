7d7065e68648ec54f15066a76e7b2d585d91d4d5
Nikita <nks.off@gmail.com>
2016-10-14 13:55:32 +0300

    add ex 2

--- ex_2.py
+++ ex_2.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-from librip.gens import gen_random
+from librip.gen import gen_random
 from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
 
 # Реализация задания 2
+print(list(Unique(data1)))
+print(list(Unique(data2)))
+print(list(Unique(['a', 'A', 'b', 'B'])))
+print(list(Unique(['a', 'A', 'b', 'B'], ignore_case=True)))
--- librip/iterators.py
+++ librip/iterators.py
@@ -1,17 +1,19 @@
 # Итератор для удаления дубликатов
 class Unique(object):
     def __init__(self, items, **kwargs):
-        # Нужно реализовать конструктор
-        # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
-        # в зависимости от значения которого будут считаться одинаковые строки в разном регистре
-        # Например: ignore_case = True, Aбв и АБВ разные строки
-        #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
-        # По-умолчанию ignore_case = False
-        pass
+        self.ignore_case = kwargs.get('ignore_case') is not None and kwargs.get('ignore_case')
+        self.items = iter(items)
+        self.seen = set()
 
     def __next__(self):
-        # Нужно реализовать __next__    
-        pass
+        val = self.items.__next__()
+        val = str(val).lower() if self.ignore_case else val
+
+        if val not in self.seen:
+            self.seen.add(val)
+            return val
+
+        return self.__next__()
 
     def __iter__(self):
         return self
