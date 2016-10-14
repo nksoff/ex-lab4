a5db5736b707ad8eb5efdd6991627ce79890bfb1
Nikita <nks.off@gmail.com>
2016-10-14 13:59:16 +0300

    add ex 6

--- ex_6.py
+++ ex_6.py
@@ -3,10 +3,10 @@ import json
 import sys
 from librip.ctxmngrs import timer
 from librip.decorators import print_result
-from librip.gens import field, gen_random
+from librip.gen import field, gen_random
 from librip.iterators import Unique as unique
 
-path = None
+path = sys.argv[1]
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
@@ -23,22 +23,23 @@ with open(path) as f:
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+    return sorted(unique(field(arg, 'job-name'), ignore_case=True))
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    return list(filter(lambda x: str(x).startswith('программист'), arg))
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+    return ["{} с опытом Python".format(i) for i in arg]
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
+    return ["{}, зарплата {} руб.".format(work, salary) for (work, salary) in
+            zip(arg, gen_random(100000, 200000, len(arg)))]
 
 
 with timer():
